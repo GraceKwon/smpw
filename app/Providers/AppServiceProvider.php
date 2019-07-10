8fb923bcbf1ea7795d2f00f3ca60ee89520ee1c8
hanzinhee <hanzinhee@gmail.com>
2019-07-10 14:36:02 +0900

    삭제 콤퍼넌트

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::component('components.breadcrumb', 'breadcrumb');
     }
 }
