5f1cfadeaa5edb55b5d0f72bac2a8c3e7c3a0fb1
hanzinhee <hanzinhee@gmail.com>
2019-07-11 14:16:30 +0900

    ...

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,8 @@
 <?php
 function set_breadcrumb_array($path_explode) {
-	$path = $path_explode[0];                    //서브 path 제거
+	$path = $path_explode[0];                    	//서브 path 제거
 	$path_count = count($path_explode);             // path 레벨 확인
+	
 	$subpage_index = 0;
 	if($path_count > 1){                           //서브페이지가 있으면
 		$subpage_index = $path_explode[1];
@@ -10,8 +11,10 @@ function set_breadcrumb_array($path_explode) {
 			$subpage_index = 1;
 		}
 	}
-	$breadcrumb = session('breadcrumb')[$path];     // rending breadcrumb array
-	$breadcrumb = array_splice($breadcrumb, 0, $path_count + 1); // $breadcrumb = ['메뉴', '서브메뉴', '서브페이지']
+
+	// $breadcrumb = [ ['path'=>null,'name'=>'메뉴'], ['path'=>'path','name'=>'서브메뉴'], ['path'=>null,'name'=>['서브페이지']]]
+	$breadcrumb = session('breadcrumb')[$path];    
+	$breadcrumb = array_splice($breadcrumb, 0, $path_count + 1); 
 	
 	if( isset($breadcrumb[2]) ){
 		if( isset($breadcrumb[2]['name'][$subpage_index])){
