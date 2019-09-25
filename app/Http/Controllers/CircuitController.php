aea88b78ab35a5f5ea681b87ff62a10b3d3fbc81
hanzinhee <hanzinhee@gmail.com>
2019-09-25 17:34:01 +0900

    순회구관리 > 구역관리 작업중

--- app/Http/Controllers/CircuitController.php
+++ app/Http/Controllers/CircuitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class CircuitController extends Controller
 {
@@ -14,10 +15,6 @@ class CircuitController extends Controller
 
     public function serviceZones(Request $request)
     {
-        $request->CirCode = '';
-        // return view( 'circuit.zones', [
-        //     'ZoneList' => DB::table('TB_Zone')->paginate(10),
-        // ]);
         return view( 'circuit.serviceZones', [
             'ServiceZoneList' => DB::select('uspGetStandingServiceZoneList'),
         ]);
@@ -25,26 +22,64 @@ class CircuitController extends Controller
 
     public function formServiceZones(Request $request)
     {
-        $ServiceZoneID = $request->ServiceZoneID;
-       
+        if( $request->ServiceZoneID !== '0' ) {
+
+            $res = DB::select( 'uspGetStandingServiceZoneDetail ?', [ $request->ServiceZoneID ] );
+            $ServiceZone = reset($res); /* reset( [] ) === false */
+            if( empty($ServiceZone) || $ServiceZone->UseYn === 0 ) abort(404); /* empty( false ) === true */
+
+        }
+
         return view( 'circuit.formServiceZones', [
-            'ServiceZoneID' => $ServiceZoneID,
-        ]);
+                'ServiceZone' => isset($ServiceZone) ? $ServiceZone : null, 
+            ]);
     }
 
     public function putServiceZones(Request $request)
     {
-        $res = DB::select('uspSetStandingServiceZoneInsert ?,?,?,?,?,?,?,?', [
-            $request->ZoneName,
-            $request->ZoneAlias,
-            $request->Latitude,
-            $request->Longitude,
-            $request->ZoneAddress,
-            $request->OrderNum,
-            session('auth.AdminID'),
-            session('auth.CircuitID')
-        ]);
-        return redirect('/ServiceZones');
+        if($request->ServiceZoneID === '0')
+            $res = DB::select('uspSetStandingServiceZoneInsert ?,?,?,?,?,?,?,?', [
+                    $request->ZoneName,
+                    $request->ZoneAlias,
+                    $request->Latitude,
+                    $request->Longitude,
+                    $request->ZoneAddress,
+                    $request->OrderNum,
+                    session('auth.AdminID'),
+                    session('auth.CircuitID')
+                ]);
+        else
+            $res = DB::select('uspSetStandingServiceZoneUpdate ?,?,?,?,?,?,?,?,?', [
+                    $request->ServiceZoneID,
+                    $request->ZoneName,
+                    $request->ZoneAlias,
+                    $request->Latitude,
+                    $request->Longitude,
+                    $request->ZoneAddress,
+                    $request->OrderNum,
+                    session('auth.AdminID'),
+                    session('auth.CircuitID')
+                ]);
+        
+
+        if(getAffectedRows($res) === 0) 
+            return back()->withErrors(['fail' => '저장 실패하였습니다.']);
+        else
+            return redirect('/ServiceZones');
+        
+    }
+
+    public function deleteServiceZones(Request $request)
+    {
+        $res = DB::select('uspSetStandingServiceZoneDelete ?', [
+                $request->ServiceZoneID,
+            ]);
+
+        if( getAffectedRows($res) === 0 ) 
+            return back()->withErrors(['fail' => '삭제 실패하였습니다.']);
+        else
+            return redirect('/ServiceZones');
+        
     }
 
     public function view_admins(Request $request)
@@ -71,7 +106,7 @@ class CircuitController extends Controller
             1, //@CircuitID
             1, //@CongregationID
             1, //@AdminName
-        ]
+        ];
         // return view( 'circuit.zones', [
         //     'ZoneList' => DB::table('TB_Zone')->paginate(10),
         // ]);
