6ba46a9988b8c5c9568f02e3faf47bb50d3f3ad1
hanzinhee <hanzinhee@gmail.com>
2019-10-10 14:43:10 +0900

    search selectBoxs

--- resources/views/circuit/admins.blade.php
+++ resources/views/circuit/admins.blade.php
@@ -1,139 +1,137 @@
 @extends('layouts.frames.master')
 @section('content')
-<form method="GET">
 
-        @include('layouts.sections.search', [
-            'inputText' => [
-                'label' => '이름',
-                'id' =>'AdminName'
-            ]
-        ])
-
-   
-</form>
-
-<section class="section-table-section">
-    <div class="table-responsive">
-        <table class="table table-center table-font-size-90">
-            <thead>
-            <tr>
-                <th>
-                    <div class="min-width">
-                        <span>No</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>도시</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>순회구</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>회중</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>이름</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>신분</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>연락처</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>권한</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>지정일자</span>
-                    </div>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-                {{-- <tr>
-                    <td>
-                        201
-                    </td>
-                    <td>
-                        남양주
-                    </td>
-                    <td>
-                        경기18
-                    </td>
-                    <td>
-                        남양주양지
-                    </td>
-                    <td>
-                        김사랑
-                    </td>
-                    <td>
-                        장로
-                    </td>
-                    <td>
-                        010-1234-5678
-                    </td>
-                    <td>
-                        보조자
-                    </td>
-                    <td>
-                        2019-03-23
-                    </td>
-                </tr> --}}
-                @foreach ($AdminList as $Admin)
-                <tr>
-                    <td>
-                        {{ $Admin->AdminID }}
-                    </td>
-                    <td>
-                        {{ $Admin->MetroName }}
-                    </td>
-                    <td>
-                        순회구
-                    </td>
-                    <td>
-                        회중
-                    </td>
-                    <td>
-                        {{ $Admin->AdminName }}
-                    </td>
-                    <td>
-                        신분
-                    </td>
-                    <td>
-                        {{ $Admin->Mobile }}
-                    </td>
-                    <td>
-                        {{ $Admin->AdminRole }}
-                    </td>
-                    <td>
-                        {{ $Admin->CreateDate }}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    @include('layouts.sections.registrationButton', [
-        'label' => '사용자 등록',
+    @include('layouts.sections.search', [
+        'inputText' => [
+            'label' => '이름',
+            'id' =>'AdminName'
+        ]
     ])
 
-    {{ $AdminList->appends( request()->all() )->links() }}
-</section>
+   
+
+    <section class="section-table-section">
+        <div class="table-responsive">
+            <table class="table table-center table-font-size-90">
+                <thead>
+                <tr>
+                    <th>
+                        <div class="min-width">
+                            <span>No</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>도시</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>순회구</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>회중</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>이름</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>신분</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>연락처</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>권한</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>지정일자</span>
+                        </div>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    {{-- <tr>
+                        <td>
+                            201
+                        </td>
+                        <td>
+                            남양주
+                        </td>
+                        <td>
+                            경기18
+                        </td>
+                        <td>
+                            남양주양지
+                        </td>
+                        <td>
+                            김사랑
+                        </td>
+                        <td>
+                            장로
+                        </td>
+                        <td>
+                            010-1234-5678
+                        </td>
+                        <td>
+                            보조자
+                        </td>
+                        <td>
+                            2019-03-23
+                        </td>
+                    </tr> --}}
+                    @foreach ($AdminList as $Admin)
+                    <tr>
+                        <td>
+                            {{ $Admin->AdminID }}
+                        </td>
+                        <td>
+                            {{ $Admin->MetroName }}
+                        </td>
+                        <td>
+                            순회구
+                        </td>
+                        <td>
+                            회중
+                        </td>
+                        <td>
+                            {{ $Admin->AdminName }}
+                        </td>
+                        <td>
+                            신분
+                        </td>
+                        <td>
+                            {{ $Admin->Mobile }}
+                        </td>
+                        <td>
+                            {{ $Admin->AdminRole }}
+                        </td>
+                        <td>
+                            {{ $Admin->CreateDate }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @include('layouts.sections.registrationButton', [
+            'label' => '사용자 등록',
+        ])
+
+        {{ $AdminList->appends( request()->all() )->links() }}
+    </section>
 @endsection
 
 @section('popup')
--- resources/views/circuit/keepZones.blade.php
+++ resources/views/circuit/keepZones.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.frames.master')
 @section('content')
-<form method="GET">
 
     @include('layouts.sections.search', [
         'inputText' => [
@@ -8,134 +7,132 @@
             'id' =>'keepZoneName'
         ]
     ])
-
-</form>
-<section class="section-table-section">
-    <div class="table-responsive">
-        <table class="table table-center table-font-size-90">
-            <thead>
-            <tr>
-                <th>
-                    <div class="min-width">
-                        <span>No</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>도시</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>순회구</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>회중</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>관리자 이름</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>신분</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>연락처</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>보관장소 주소</span>
-                    </div>
-                </th>
-                <th>
-                    <div class="min-width">
-                        <span>지정일자</span>
-                    </div>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-                {{-- <tr>
-                    <td>
-                        201
-                    </td>
-                    <td>
-                        남양주
-                    </td>
-                    <td>
-                        경기18
-                    </td>
-                    <td>
-                        남양주양지
-                    </td>
-                    <td>
-                        김사랑
-                    </td>
-                    <td>
-                        장로
-                    </td>
-                    <td>
-                        010-1234-5678
-                    </td>
-                    <td>
-                        서울 송파구 문정동 324-942 2층
-                    </td>
-                    <td>
-                        2019-03-23
-                    </td>
-                </tr> --}}
-                @foreach ($KeepZoneList as $KeppZone)
+    <section class="section-table-section">
+        <div class="table-responsive">
+            <table class="table table-center table-font-size-90">
+                <thead>
                 <tr>
-                    <td>
-                        {{-- {{ $KeppZone->KeepZoneID }} --}}
-                    </td>
-                    <td>
-                        {{ $KeppZone->MetroName }}
-                    </td>
-                    <td>
-                        {{ $KeppZone->CircuitName }}
-                    </td>
-                    <td>
-                        {{-- {{ $KeppZone->CongregationName }} --}}
-                    </td>
-                    <td>
-                        {{ $KeppZone->AdminName }}
-                    </td>
-                    <td>
-                        장로
-                    </td>
-                    <td>
-                        {{ $KeppZone->Mobile }}
-                    </td>
-                    <td>
-                        {{ $KeppZone->ZoneAddress }}
-                    </td>
-                    <td>
-                        {{ $KeppZone->CreateDate }}
-                    </td>
+                    <th>
+                        <div class="min-width">
+                            <span>No</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>도시</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>순회구</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>회중</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>관리자 이름</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>신분</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>연락처</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>보관장소 주소</span>
+                        </div>
+                    </th>
+                    <th>
+                        <div class="min-width">
+                            <span>지정일자</span>
+                        </div>
+                    </th>
                 </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    @if(session('auth.CircuitID'))
-        @include('layouts.sections.registrationButton', [
-            'label' => '보관장소 등록',
-        ])
-    @endif
+                </thead>
+                <tbody>
+                    {{-- <tr>
+                        <td>
+                            201
+                        </td>
+                        <td>
+                            남양주
+                        </td>
+                        <td>
+                            경기18
+                        </td>
+                        <td>
+                            남양주양지
+                        </td>
+                        <td>
+                            김사랑
+                        </td>
+                        <td>
+                            장로
+                        </td>
+                        <td>
+                            010-1234-5678
+                        </td>
+                        <td>
+                            서울 송파구 문정동 324-942 2층
+                        </td>
+                        <td>
+                            2019-03-23
+                        </td>
+                    </tr> --}}
+                    @foreach ($KeepZoneList as $KeppZone)
+                    <tr>
+                        <td>
+                            {{-- {{ $KeppZone->KeepZoneID }} --}}
+                        </td>
+                        <td>
+                            {{ $KeppZone->MetroName }}
+                        </td>
+                        <td>
+                            {{ $KeppZone->CircuitName }}
+                        </td>
+                        <td>
+                            {{-- {{ $KeppZone->CongregationName }} --}}
+                        </td>
+                        <td>
+                            {{ $KeppZone->AdminName }}
+                        </td>
+                        <td>
+                            장로
+                        </td>
+                        <td>
+                            {{ $KeppZone->Mobile }}
+                        </td>
+                        <td>
+                            {{ $KeppZone->ZoneAddress }}
+                        </td>
+                        <td>
+                            {{ $KeppZone->CreateDate }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @if(session('auth.CircuitID'))
+            @include('layouts.sections.registrationButton', [
+                'label' => '보관장소 등록',
+            ])
+        @endif
 
 
-    {{ $KeepZoneList->appends( request()->all() )->links() }}
+        {{ $KeepZoneList->appends( request()->all() )->links() }}
 
-</section>
+    </section>
 @endsection
 
 @section('popup')
