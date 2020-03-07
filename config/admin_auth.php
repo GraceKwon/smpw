ffdbb4a5ba53f18506e2fe0deaca1ae100421b5c
hanzinhee <hanzinhee@gmail.com>
2020-03-07 19:45:29 +0900

    ...

--- config/admin_auth.php
+++ config/admin_auth.php
@@ -92,15 +92,15 @@ return [
             'statistic-publishers' => [
                 'name' => '봉사자통계',
                 'auth' => [1,2,3,4,5]
-            ],
-            'statistics-reports' => [
-                'name' => '봉사보고통계',
-                'auth' => [1,2,3,4,5]
-            ],
-            'statistics-products' => [
-                'name' => '출판물배부통계',
-                'auth' => [1,2,3,4,5]
             ]
+            // 'statistics-reports' => [
+            //     'name' => '봉사보고통계',
+            //     'auth' => [1,2,3,4,5]
+            // ],
+            // 'statistics-products' => [
+            //     'name' => '출판물배부통계',
+            //     'auth' => [1,2,3,4,5]
+            // ]
         ]
     ],
 
