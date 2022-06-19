192e66da216fc34a03bf6dabecf1c7de4f2dba00
gracek <di9931001@gmail.com>
2022-06-19 21:04:16 +0900

    카카오 맵 API 키 변경

--- resources/views/circuit/formServiceZones.blade.php
+++ resources/views/circuit/formServiceZones.blade.php
@@ -5,7 +5,7 @@
         <div class="alert alert-danger">{!! $message !!}</div>
     @enderror
     <form method="POST"
-        @submit="_confirm" 
+        @submit="_confirm"
         @keydown.enter.prevent>
         @method("PUT")
         @csrf
@@ -17,7 +17,7 @@
                 </th>
                 <td>
                     <div class="inline-responsive">
-                        <select class="custom-select @error('OrderNum') is-invalid @enderror" 
+                        <select class="custom-select @error('OrderNum') is-invalid @enderror"
                             id="OrderNum"
                             name="OrderNum"
                             v-model="OrderNum">
@@ -47,8 +47,8 @@
                     <label class="label" for="ZoneAlias">구역 약호</label>
                 </th>
                 <td>
-                    <input type="text" 
-                        class="form-control min-w-300px-desktop @error('ZoneAlias') is-invalid @enderror" 
+                    <input type="text"
+                        class="form-control min-w-300px-desktop @error('ZoneAlias') is-invalid @enderror"
                         id="ZoneAlias"
                         name="ZoneAlias"
                         v-model="ZoneAlias"
@@ -63,8 +63,8 @@
                     <label class="label" for="ZoneName">구역 명칭</label>
                 </th>
                 <td colspan="3">
-                    <input type="text" 
-                        class="form-control min-w-300px-desktop  @error('ZoneName') is-invalid @enderror" 
+                    <input type="text"
+                        class="form-control min-w-300px-desktop  @error('ZoneName') is-invalid @enderror"
                         id="ZoneName"
                         name="ZoneName"
                         v-model="ZoneName"
@@ -80,12 +80,12 @@
                 </th>
                 <td>
                     <div class="register-form-container inline-responsive">
-                        <input type="text" 
-                            class="form-control min-w-300px-desktop  @error('Latitude') is-invalid @enderror" 
-                            id="Latitude" 
-                            name="Latitude" 
-                            v-model="Latitude" 
-                            placeholder="지도에 선택된 구역의 위도가 표시됩니다." 
+                        <input type="text"
+                            class="form-control min-w-300px-desktop  @error('Latitude') is-invalid @enderror"
+                            id="Latitude"
+                            name="Latitude"
+                            v-model="Latitude"
+                            placeholder="지도에 선택된 구역의 위도가 표시됩니다."
                             readonly>
                         @error('Latitude')
                             <div class="invalid-feedback">{{ $message }}</div>
@@ -97,12 +97,12 @@
                 </th>
                 <td>
                     <div class="register-form-container inline-responsive">
-                        <input type="text" 
-                            class="form-control min-w-300px-desktop  @error('Longitude') is-invalid @enderror" 
-                            id="Longitude" 
-                            name="Longitude" 
-                            v-model="Longitude" 
-                            placeholder="지도에 선택된 구역의 경도가 표시됩니다." 
+                        <input type="text"
+                            class="form-control min-w-300px-desktop  @error('Longitude') is-invalid @enderror"
+                            id="Longitude"
+                            name="Longitude"
+                            v-model="Longitude"
+                            placeholder="지도에 선택된 구역의 경도가 표시됩니다."
                             readonly>
                         @error('Longitude')
                             <div class="invalid-feedback">{{ $message }}</div>
@@ -117,10 +117,10 @@
                 <td colspan="3" style="position:relative">
                     <div ref="map" class="map p-3 text-muted font-size-80"></div>
                     <div>
-                        <input type="text" placeholder="주소만 입력 가능합니다." 
-                            @keyup.enter="_addressSearch" 
+                        <input type="text" placeholder="주소만 입력 가능합니다."
+                            @keyup.enter="_addressSearch"
                             v-model="search">
-                        <button type="button" class="btn-xsm btn-primary" 
+                        <button type="button" class="btn-xsm btn-primary"
                             @click="_addressSearch">검색</button>
                         <span class="text-center" v-html="alertMessage"></span>
                     </div>
@@ -131,12 +131,12 @@
                     <label class="label" for="ZoneAddress">구역 주소</label>
                 </th>
                 <td colspan="3">
-                    <input type="text" 
-                        class="form-control @error('ZoneAddress') is-invalid @enderror" 
-                        id="ZoneAddress" 
-                        name="ZoneAddress" 
-                        v-model="ZoneAddress" 
-                        placeholder="지도에 선택된 구역의 주소가 표시됩니다." 
+                    <input type="text"
+                        class="form-control @error('ZoneAddress') is-invalid @enderror"
+                        id="ZoneAddress"
+                        name="ZoneAddress"
+                        v-model="ZoneAddress"
+                        placeholder="지도에 선택된 구역의 주소가 표시됩니다."
                         readonly>
                     @error('ZoneAddress')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -149,7 +149,7 @@
         @include('layouts.sections.formButton', [
             'id' => isset($ServiceZone->ServiceZoneID),
         ])
-        
+
     </form>
     <form ref="formDelete" method="POST">
         @method("DELETE")
@@ -159,7 +159,7 @@
 @endsection
 
 @section('script')
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=10f0647488c1c161a2bb5cbc32269402&libraries=services"></script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1a62bf5219322c8e2986927437b48d8d&libraries=services"></script>
 <script>
     var app = new Vue({
         el:'#wrapper-body',
@@ -181,23 +181,23 @@
         },
         mounted: function () {
             this._loadKakaoMap()
-        
+
             // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-            kakao.maps.event.addListener(this.map, 'click', 
-                function(mouseEvent) {        
-                    // 클릭한 위도, 경도 정보를 가져옵니다 
-                    var latlng = mouseEvent.latLng; 
-                    
+            kakao.maps.event.addListener(this.map, 'click',
+                function(mouseEvent) {
+                    // 클릭한 위도, 경도 정보를 가져옵니다
+                    var latlng = mouseEvent.latLng;
+
                     // 마커 위치를 클릭한 위치로 옮깁니다
                     this.marker.setPosition(latlng);
                     this.marker.setMap(this.map);
-                    
-                    this.geocoder.coord2Address(latlng.getLng(), latlng.getLat(), 
+
+                    this.geocoder.coord2Address(latlng.getLng(), latlng.getLat(),
                         function(result, status) {
                             var address = '';
                             if (status === kakao.maps.services.Status.OK) {
-                                address = result[0].road_address ? 
-                                    result[0].road_address.address_name + '(도로명)' : 
+                                address = result[0].road_address ?
+                                    result[0].road_address.address_name + '(도로명)' :
                                     result[0].address.address_name + '(지번)';
                             }
                             // document.getElementById('ZoneAddress').value = address;
@@ -207,7 +207,7 @@
                         // document.getElementById('Longitude').value = latlng.getLng();
                         this.Latitude = latlng.getLat();
                         this.Longitude = latlng.getLng();
-                    
+
                 }.bind(this));
 
         },
@@ -224,24 +224,24 @@
                 var zoomControl = new kakao.maps.ZoomControl(); // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
                 this.map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
                 this.map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-                
+
                 this.geocoder = new kakao.maps.services.Geocoder(); // 주소-좌표 변환 객체를 생성합니다
-                
+
 
                 this.map.setCenter(new kakao.maps.LatLng(this.Latitude, this.Longitude))
                 this.marker = new kakao.maps.Marker({
                     position: new kakao.maps.LatLng(this.Latitude, this.Longitude)
                 }); //마커 객체를 생성합니다.
-                
-                this.marker.setMap(this.map);  
+
+                this.marker.setMap(this.map);
 
             },
             _addressSearch: function() {
                 var alertMessage = '';
 
-                this.geocoder.addressSearch(this.search, 
+                this.geocoder.addressSearch(this.search,
                     function(result, status) {
-                        // 정상적으로 검색이 완료됐으면 
+                        // 정상적으로 검색이 완료됐으면
                         if (status === kakao.maps.services.Status.OK) {
 
                             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
@@ -252,14 +252,14 @@
                             alertMessage = '검색결과가 없습니다.'
                         }
                         this.alertMessage = alertMessage;
-                    }.bind(this));  
+                    }.bind(this));
             },
             _confirm: function (e) {
                 var res = confirm('{{ isset($ServiceZone->ServiceZoneID) ? '수정' : '저장' }} 하시겠습니까?');
                 if(!res){
                     e.preventDefault();
                 }
-                
+
             },
             _delete: function () {
                 console.log(this.$refs.formRemove);
