58c5dda4f1614fefc07f72107755bca845350dd6
gracek <di9931001@gmail.com>
2022-06-21 01:10:18 +0900

    FCM key 변경

--- config/fcm.php
+++ config/fcm.php
@@ -5,8 +5,8 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => env('FCM_SERVER_KEY', 'AAAAo_T9tUA:APA91bEyfid9gKsFJduU9Su02pBYEBFRaUB1lZ-b3KmPWvXdHvOHFef_pUx_hb0ytLoBvQ5i08X_1NZaMv2fIFh_vaNOQoNJGW7U1CmiJm-AqUS2oDhkhF4lZel8IRWo43nfRtuPeaul'),
-        'sender_id' => env('FCM_SENDER_ID', '704189936960'),
+        'server_key' => env('FCM_SERVER_KEY', 'AAAAUUOOk7I:APA91bE3GfIQ1BrVF7dwKkLOhbXL3JWinh4Lvbcp9zECdaZnpIWmOlqT_OXg6a5J_FgpAn-9H6ABmJ4FFDrq6orzBBL-gMqh_osEf8teLhzu1slRRabKb1aIoFoxZMC0AcYcdUGbrvv8'),
+        'sender_id' => env('FCM_SENDER_ID', '349025768370'),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
