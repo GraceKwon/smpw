e936edcd45502a60039df7965164cc98ade74e9b
k-lunasoft <ehkwon@lunasoft.co.kr>
2022-10-18 14:14:32 +0900

    fcm 키 변경

--- config/fcm.php
+++ config/fcm.php
@@ -5,8 +5,8 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => env('FCM_SERVER_KEY', 'AAAAUUOOk7I:APA91bE3GfIQ1BrVF7dwKkLOhbXL3JWinh4Lvbcp9zECdaZnpIWmOlqT_OXg6a5J_FgpAn-9H6ABmJ4FFDrq6orzBBL-gMqh_osEf8teLhzu1slRRabKb1aIoFoxZMC0AcYcdUGbrvv8'),
-        'sender_id' => env('FCM_SENDER_ID', '349025768370'),
+        'server_key' => env('FCM_SERVER_KEY', 'AAAAo_T9tUA:APA91bFNmgmsufceVxAo42Slcl_OphOgz8hsAbxrvtan_VeFpZQSZIWNV3JNQxkCt6zK9oH1vt5VmGseqBEVHhU2HxHSyr3LSXFrFf0ekCMYhha1AtcKK-i0IsvLdw24V09vocHeVzfi'),
+        'sender_id' => env('FCM_SENDER_ID', '704189936960'),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
