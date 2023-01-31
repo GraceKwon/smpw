c94716f191d30a721bfdb898cab08ad8de95ddcd
gracek <di9931001@gmail.com>
2023-01-31 23:31:53 +0900

    ip 받는 부분 확인

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -19,10 +19,28 @@ class Localization
      */
     public function handle(Request $request, Closure $next)
     {
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        } else if(getenv('HTTP_X_FORWARDED_FOR')) {
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        } else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+
+
         $checkIp = $_SERVER['REMOTE_ADDR'];
 //        $checkIp = '211.234.188.150';
 //        $checkIp = '196.201.141.224';
         $getLocation = Location::get($checkIp);
+        \Log::info($ipaddress);
+        \Log::info('=========OTHER===========');
         \Log::info($checkIp);
         \Log::info('======IP===========');
         \Log::info($getLocation);
