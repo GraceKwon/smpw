8fc807dd242f0c69948b05d083c119d996f7251a
gracek <di9931001@gmail.com>
2024-09-05 01:31:32 +0900

    feat : fcm http -> http v1 마이그레이션 end point 변경

--- config/fcm.php
+++ config/fcm.php
@@ -7,7 +7,7 @@ return [
     'http' => [
         'server_key' => env('FCM_SERVER_KEY', 'AAAAo_T9tUA:APA91bFNmgmsufceVxAo42Slcl_OphOgz8hsAbxrvtan_VeFpZQSZIWNV3JNQxkCt6zK9oH1vt5VmGseqBEVHhU2HxHSyr3LSXFrFf0ekCMYhha1AtcKK-i0IsvLdw24V09vocHeVzfi'),
         'sender_id' => env('FCM_SENDER_ID', '704189936960'),
-        'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
+        'server_send_url' => 'https://fcm.googleapis.com/v1/projects/smpw-7d126/messages:send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
     ],
